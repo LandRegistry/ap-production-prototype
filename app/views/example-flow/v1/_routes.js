f7a90e89ccaa3eeeb581d5125cf98138b6c87120
Andy Porter <andy.porter@digital.landregistry.gov.uk>
2023-04-19 18:29:29 +0100

    preliminary page chare as per v8 old proto

--- app/views/example-flow/v1/_routes.js
+++ app/views/example-flow/v1/_routes.js
@@ -16,7 +16,11 @@ router.post('/00-worklist', function (req, res) {
 })
 
 router.post('/01-preliminary', function (req, res) {
-  if (req.session.data['property-match'] == 'No' || req.session.data['customer-match'] == 'No') {
+  if (req.session.data['property-match'] == 'No' 
+  	|| req.session.data['customer-match'] == 'No'
+  	|| req.session.data['id-check'] == 'No'
+  	|| req.session.data['conveyancer-match'] == 'No'
+  	) { 	
     res.redirect('workflow-no-drafting');
   } else {
     res.redirect('questions');
@@ -28,6 +32,23 @@ router.post('/questions', function (req, res) {
     res.redirect('complete--sent-to-markoff');
   } else {
     res.redirect('complete--sent-to-workflow');
+  }
+})
+
+
+// last page in flow - route to workflow or success confirmation page
+router.post('/final-page-in-flow', function (req, res) {
+
+	// check all the questions that mean it gets sent to workflow
+  if 
+  	(
+  				req.session.data['question'] == 'No'
+			|| 	req.session.data['other-question'] == 'No'
+  	) 
+  {
+    res.redirect('workflow');
+  } else {
+    res.redirect('success');
   }
 })
 
