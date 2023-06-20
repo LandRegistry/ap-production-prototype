ce2e51e7c7639cee843a5a060d45f41304d2475e
Andy Porter <andy.porter@digital.landregistry.gov.uk>
2023-06-20 16:53:37 +0100

    initialise flow - copy of charges

--- app/routes.js
+++ app/routes.js
@@ -18,8 +18,16 @@ router.use(/\/example-flow\/v([0-9]+)/, (req, res, next) => {
 })
 // ***********************************************
 
-// route for the address for service prototype
+
+// routing for the address for service prototype
 
 router.use(/\/address-proto\/v([0-9]+)/, (req, res, next) => {
   return require(`./views/address-proto/v${req.params[0]}/_routes`)(req, res, next);
+})
+
+
+// routing for the deferral for dishcarges - transfer-defer
+
+router.use(/\/transfer-defer\/v([0-9]+)/, (req, res, next) => {
+  return require(`./views/transfer-defer/v${req.params[0]}/_routes`)(req, res, next);
 })
