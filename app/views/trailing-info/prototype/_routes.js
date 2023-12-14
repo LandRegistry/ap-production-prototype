0b3c8b8ac6f075c738c7b4fc2ed5cb89d1ac3023
Darren H <darren.harridence@landregistry.gov.uk>
2023-12-14 19:02:20 +0000

    complex error msgs scenarios build

--- app/views/trailing-info/prototype/_routes.js
+++ app/views/trailing-info/prototype/_routes.js
@@ -6,39 +6,66 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+//if var |length
 
 router.post('/trailing-info_1-1', function (req, res) {
-    var theRadios1 = req.session.data['CONFIRM-TRANSFEREE-APPLICANTS_transferee-0']
 
-    if (req.session.data['CONFIRM-TRANSFEREE-APPLICANTS_add_transferee'] == 'Add transferee') {
+    if (req.session.data['add_transferee_btn'] == 'Add transferee') {
 
         res.redirect("trailing-info_1-1-add-transferee")
 
-    } else if (theRadios1 == null){
+    } else if (req.session.data['CONFIRM-TRANSFEREE-APPLICANTS_transferee-0'] == null || req.session.data['forenames0'] == '' || req.session.data['surname0'] == '') {
 
         res.redirect("trailing-info_1-1-error-radios")
 
-    } else if (theRadios1 == 'PRIVATE_INDIVIDUAL') {
-        
-        if( (req.session.data['forenames0'] == '') && (req.session.data['surname0'] == '') ){
-            res.redirect("trailing-info_1-1-error-forenames0-and-lastname0")
+    } else {
 
-        } else if(req.session.data['forenames0'] == ''){
-            res.redirect("trailing-info_1-1-error-forenames0")
+        res.redirect("xxxxxx")
 
-        } else if (req.session.data['surname0'] == ''){
-            res.redirect("trailing-info_1-1-error-lastname0")
-        }
-
-    } else if (theRadios1 == 'UK_COMPANY') {
-        res.redirect("trailing-info_1-1-two")
-    } 
-    
-    /* error messaging not added to the second Transferee 2 form (yet?) */
+    }
 })
 
+router.post('/trailing-info_1-1-error-radios', function (req, res) {
+
+    if (req.session.data['add_transferee_btn'] == 'Add transferee') {
+
+        res.redirect("trailing-info_1-1-add-transferee-error-radios")
+
+    } else if (req.session.data['CONFIRM-TRANSFEREE-APPLICANTS_transferee-0'] == null || req.session.data['forenames0'] == '' || req.session.data['surname0'] == '') {
+
+        res.redirect("trailing-info_1-1-error-radios")
+
+    } else {
+
+        res.redirect("xxxxxxzzzzz")
+
+    }
+})
+
+
+
+
+/* can delete trailing-infor_1-1-add-transferee ????
+
 router.post('/trailing-info_1-1-add-transferee', function (req, res) {
-    //res.redirect('/index');
+    
+    if(req.session.data['CONFIRM-TRANSFEREE-APPLICANTS_add_transferee'] == 'Add transferee'){   
+        res.redirect("trailing-info_1-1-add-transferee")
+    } else {
+        
+        if( (req.session.data['forenames0'] == '') || (req.session.data['surname0'] == '') ){
+            res.redirect("trailing-info_1-1-add-transferee-error-radios")
+        } else if( (req.session.data['forenames1'] == '') || (req.session.data['surname1'] == '') ){
+            res.redirect("trailing-info_1-1-add-transferee-error-radios")
+        } else {
+            res.redirect("trailing-info_1-2")
+        }
+
+    }
+
+//trailing-info_1-1-add-transferee-error-radios.html
+*/
+
 
     //res.redirect('trailing-info_1-3');
 
